ec251e3bebddfe4f5db39cc48d7c791302c9195a
furu <githubfu093@gmail.com>
2020-03-07 14:48:45 +0000

    Schedule登録機能実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,10 +2,26 @@ class SchedulesController < ApplicationController
   
   before_action :require_user_logged_in
   
+  before_action :correct_user, only: [:destroy]
+  
   def new
+    
+    @schedule = Schedule.new
+    
   end
 
   def create
+    
+    @schedule = current_user.schedules.build(schedule_params)
+    
+      if @schedule.save
+        flash[:success] = "スケジュールの投稿に成功しました。"
+        redirect_to schedules_path
+      else  
+        flash.now[:danger] ="スケジュールの投稿に失敗しました。"
+        render :new
+      end
+  
   end
 
   def show
@@ -25,4 +41,11 @@ class SchedulesController < ApplicationController
 
   def destroy
   end
+  
+  def schedule_params
+    
+   params.require(:schedule).permit(:title, :body, :place, :start_time, :end_time)
+  
+  end
+  
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,10 @@ class Schedule < ApplicationRecord
     
   belongs_to :user
   
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :place, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  
 end
