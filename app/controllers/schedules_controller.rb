7f00fd091cd52de2e3bf0bb7de1cb4663e5e54f3
furu <githubfu093@gmail.com>
2020-03-08 06:25:16 +0000

    編集・削除機能追加

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,7 +2,7 @@ class SchedulesController < ApplicationController
   
   before_action :require_user_logged_in
   
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :show, :update, :destroy]
   
   def new
     
@@ -34,18 +34,50 @@ class SchedulesController < ApplicationController
   end
 
   def edit
+    
+    @schedule = Schedule.find(params[:id])
+    
   end
 
   def update
+    
+    if @schedule.update(schedule_params)
+      flash[:success] = "スケジュールの更新に成功しました。"  
+      redirect_to @schedule
+    else
+      flash.now[:danger] = "スケジュールが更新出来ませんでした。"
+      render :edit
+    end
+    
   end
 
   def destroy
+    
+    if @schedule.destroy
+      flash[:success] = "スケジュールを削除しました。"
+      redirect_to schedules_path
+    end
+    
   end
+  
+  #Strongparameter
+  
+  private
   
   def schedule_params
     
    params.require(:schedule).permit(:title, :body, :place, :start_time, :end_time)
   
+  end
+  
+  def correct_user
+  
+    @schedule = current_user.schedules.find_by(id: params[:id])
+    
+    unless @schedule
+      redirect_to schedules_path
+    end
+    
   end
   
 end
