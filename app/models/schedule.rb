e4dac6167f876e459be7ca78d238ecf6b97716ba
furu <githubfu093@gmail.com>
2020-03-21 02:49:05 +0000

    ナビバーリンクとScheduleモデルのバリデーションの修正

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,9 +2,9 @@ class Schedule < ApplicationRecord
     
   belongs_to :user
   
-  validates :title, presence: true
-  validates :body, presence: true
-  validates :place, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 20 }
+  validates :place, presence: true, length: { maximum: 20 }
   validates :start_time, presence: true
   validates :end_time, presence: true
   
